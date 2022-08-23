a49f1d2b35a3d4029f5c9d244767e84883c47213
Nived <nivroh2016@gmail.com>
2022-08-23 17:12:54 +0530

    feat: add colors

---
+++ lua/monokai-palenight/colors.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.colors()
+  return {
+    bg = "#282d42", -- or #2d3144
+    fg = "#Bac3db",
+    red = "#ff6188",
+    blue = "#78dce8",
+    green = "#a9dc76",
+    yellow = "#ffd866",
+    purple = "#ab9df2",
+    orange = "#fc9867",
+    white = "#f0ecec", 
+    comment = "#727a9c"
+}
+
+
+
+
+
+
+
