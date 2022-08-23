17cdf98bc7161732ed69ccca7d55a8e124faff9b
Nived <nivroh2016@gmail.com>
2022-08-23 19:37:19 +0530

    feat: add highlight function

---
+++ lua/monokai-palenight/highlights.lua
@@ -0,0 +1,13 @@
+local M = {}
+local hi = vim.api.nvim_set_hl
+local config = require('monokai-palenight.config')
+
+function M.set_highlight(colors, opts)
+  local highlights = config.highlights(colors)
+  for group, properties in pairs(highlights) do
+    hi(0, group, properties)
+  end
+end
+
+return M
+
