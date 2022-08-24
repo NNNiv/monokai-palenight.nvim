a65668d2abb1290e328dad15623a7ce19da2019b
Nived <nivroh2016@gmail.com>
2022-08-24 09:26:25 +0530

    add more highlights

--- lua/monokai-palenight/config.lua
+++ lua/monokai-palenight/config.lua
@@ -2,7 +2,30 @@ local M = {}
 
 function M.highlights(colors)
   return {
-    Normal = { fg = colors.fg, bg = colors.bg }
+    Normal = { fg = colors.fg, bg = colors.bg },
+    Visual = { bg = colors.select },
+    Search = { fg = colors.bg, bg = colors.fg },
+    IncSearch = { fg = colors.bg, bg = colors.fg },
+    LineNr = { fg = colors.grey },
+    CursorLineNr = { fg = colors.fg }, 
+    CursorLine = { bg = colors.select },
+    
+
+
+    NonText = { fg = colors.bg },
+    -- Syntax
+    Variable = { fg = colors.fg },
+    String = { fg = colors.yellow },
+    Character = { fg = colors.yellow },
+    Constant = { fg = colors.purple },
+    Comment = { fg = colors.comment },
+    Boolean = { fg = colors.purple }, 
+    Number = { fg = colors.purple },
+    Statement = { fg = colors.red },
+    Operator = { fg = colors.fg },
+    PreProc = { fg = colors.red },
+    Function = { fg = colors.green },
+    Special = { fg = colors.yellow },
   }
 end 
 
