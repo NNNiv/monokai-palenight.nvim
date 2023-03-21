f80abe3423047a7f24bf076f9560a7217a51bd22
NNNived <nivroh2016@gmail.com>
2023-03-21 23:49:17 +0530

    change section highlights

--- lua/lualine/themes/monokai-palenight.lua
+++ lua/lualine/themes/monokai-palenight.lua
@@ -6,23 +6,27 @@ return {
     a = { fg = colors.bg, bg = colors.red, gui = 'bold' },
     b = { fg = colors.bg, bg = colors.grey },
     c = { fg = colors.bg, bg = colors.comment},
-  },  
+  },
   normal = {
     a = { fg = colors.bg, bg = colors.blue, gui = 'bold' },
-    b = { fg = colors.bg, bg = colors.comment },
+    b = { fg = colors.bg, bg = colors.red},
     c = { fg = colors.bg, bg = colors.purple },
   },
   visual = {
     a = { fg = colors.bg, bg = colors.purple, gui = 'bold' },
-    b = { fg = colors.bg, bg = colors.comment },
+    b = { fg = colors.bg, bg = colors.red },
+    c = { fg = colors.bg, bg = colors.purple },
   },
   replace = {
     a = { fg = colors.bg, bg = colors.orange, gui = 'bold' },
-    b = { fg = colors.bg, bg = colors.comment },
+    b = { fg = colors.bg, bg = colors.red },
+    c = { fg = colors.bg, bg = colors.purple },
   },
   insert = {
     a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
-    b = { fg = colors.bg, bg = colors.comment },
-  }
+    b = { fg = colors.bg, bg = colors.red },
+    c = { fg = colors.bg, bg = colors.purple },
+  },
+
 }
 
