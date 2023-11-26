b8a7b4566f99b530d848d8b7cc907fef23777834
nnniv <nivroh2016@gmail.com>
2023-11-26 15:47:38 +0530

    enforce stylua

--- lua/monokai-palenight/highlights.lua
+++ lua/monokai-palenight/highlights.lua
@@ -1,13 +1,12 @@
 local M = {}
 local hi = vim.api.nvim_set_hl
-local config = require('monokai-palenight.config')
+local config = require("monokai-palenight.config")
 
 function M.set_highlight(colors, opts)
-  local highlights = config.highlights(colors)
-  for group, properties in pairs(highlights) do
-    hi(0, group, properties)
-  end
+	local highlights = config.highlights(colors)
+	for group, properties in pairs(highlights) do
+		hi(0, group, properties)
+	end
 end
 
 return M
-
